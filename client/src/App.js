9cd631b4464c88aec113491bbf595a9f3d07717c
Imon Ovbude <imonovbude@gmail.com>
2019-02-21 13:20:07 -0800

    added proxy to package.json file in client directory

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/" component={Posts} />
+        <Route path="/post" component={Posts} />
       </div>
     );
   }
